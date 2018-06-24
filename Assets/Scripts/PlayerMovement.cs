666fcd5e372bf15b07faa3791c51891b79a3bfa8
Mohamed Adel <mohamed.adel@eye-ltd.com>
2018-06-24 16:56:41 +0200

    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float moveSpeed = 100f;
+    [SerializeField]
+    private float turnSpeed = 5;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update () {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed );
+
+        animator.SetFloat("Speed", movement.magnitude);
+
+        if (movement.magnitude > 0) // check if movement axis is really on  move
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            //transform.rotation = newDirection; // snap rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed); // smoothe rotation
+        }
+    }
+}
